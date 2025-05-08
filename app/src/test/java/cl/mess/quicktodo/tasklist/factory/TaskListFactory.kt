9f70e25d6536b714d53abe83a0aed931af422666
Mario Sánchez S. <marsanchezs@gmail.com>
2025-05-07 21:39:31 -0400

    domain layer unit test

--- app/src/test/java/cl/mess/quicktodo/tasklist/factory/TaskListFactory.kt
+++ app/src/test/java/cl/mess/quicktodo/tasklist/factory/TaskListFactory.kt
@@ -1,11 +1,21 @@
 package cl.mess.quicktodo.tasklist.factory
 
 import cl.mess.quicktodo.tasklist.data.remote.model.RemoteTask
+import cl.mess.quicktodo.tasklist.domain.model.Task
 import cl.mess.quicktodo.utils.RandomFactory
 
 object TaskListFactory {
     fun makeRemoteTasks() = listOf(
         RemoteTask(
+            userId = RandomFactory.generateRandomInt(),
+            id = RandomFactory.generateRandomInt(),
+            title = RandomFactory.generateRandomString(),
+            completed = RandomFactory.generateRandomBoolean()
+        )
+    )
+
+    fun makeTasks() = listOf(
+        Task(
             userId = RandomFactory.generateRandomInt(),
             id = RandomFactory.generateRandomInt(),
             title = RandomFactory.generateRandomString(),
